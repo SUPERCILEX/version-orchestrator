0da6b61aaa4825f0161565cfb4e45324c2495fcd
Alex Saveau <saveau.alexandre@gmail.com>
2020-02-21 19:48:45 -0800

    Get rid of hash computation task (#2)
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- plugin/src/main/kotlin/com/supercilex/gradle/versions/tasks/ComputeVersionsTask.kt
+++ plugin/src/main/kotlin/com/supercilex/gradle/versions/tasks/ComputeVersionsTask.kt
@@ -3,13 +3,8 @@ package com.supercilex.gradle.versions.tasks
 import com.supercilex.gradle.versions.internal.execWithOutput
 import com.supercilex.gradle.versions.internal.safeCreateNewFile
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.CacheableTask
-import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputFile
-import org.gradle.api.tasks.PathSensitive
-import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.submit
 import org.gradle.kotlin.dsl.support.serviceOf
@@ -19,23 +14,22 @@ import org.gradle.workers.WorkParameters
 import org.gradle.workers.WorkerExecutor
 import javax.inject.Inject
 
-@CacheableTask
 internal abstract class ComputeVersionsTask : DefaultTask() {
-    @get:PathSensitive(PathSensitivity.RELATIVE)
-    @get:InputDirectory
-    abstract val revDir: DirectoryProperty
-
     @get:OutputFile
     abstract val versionCodeFile: RegularFileProperty
 
     @get:OutputFile
     abstract val versionNameFile: RegularFileProperty
 
+    init {
+        // We don't know what's changed on the Git side of things
+        outputs.upToDateWhen { false }
+    }
+
     @TaskAction
     fun computeVersions() {
         val executor = project.serviceOf<WorkerExecutor>()
         executor.noIsolation().submit(Computer::class) {
-            hash.set(revDir.file("current-git-rev.txt"))
             versionCode.set(versionCodeFile)
             versionName.set(versionNameFile)
         }
@@ -46,7 +40,6 @@ internal abstract class ComputeVersionsTask : DefaultTask() {
     ) : WorkAction<Computer.Params> {
         override fun execute() {
             executor.noIsolation().submit(VersionCodeComputer::class) {
-                hash.set(parameters.hash)
                 versionCode.set(parameters.versionCode)
             }
             executor.noIsolation().submit(VersionNameComputer::class) {
@@ -55,7 +48,6 @@ internal abstract class ComputeVersionsTask : DefaultTask() {
         }
 
         interface Params : WorkParameters {
-            val hash: RegularFileProperty
             val versionCode: RegularFileProperty
             val versionName: RegularFileProperty
         }
@@ -65,16 +57,14 @@ internal abstract class ComputeVersionsTask : DefaultTask() {
             private val execOps: ExecOperations
     ) : WorkAction<VersionCodeComputer.Params> {
         override fun execute() {
-            val hash = parameters.hash.get().asFile.readText()
             val versionCode = execOps.execWithOutput {
-                commandLine("git", "rev-list", "--count", hash)
+                commandLine("git", "rev-list", "--count", "HEAD")
             }
 
             parameters.versionCode.get().asFile.safeCreateNewFile().writeText(versionCode)
         }
 
         interface Params : WorkParameters {
-            val hash: RegularFileProperty
             val versionCode: RegularFileProperty
         }
     }
